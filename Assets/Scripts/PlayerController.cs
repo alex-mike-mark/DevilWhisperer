71c9ac1de367f2f7210a2248b4e996a832850f25
KeroseneCity <alex.mike.mark>
2017-08-25 16:00:54 -0500

    Create grid locked movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	//Movement stuff unashamededly stolen from robertbu's suggestion in stackoverflow question
+	//<http://answers.unity3d.com/questions/611343/movement-2d-in-a-grid.html>
+	private float speed = 2.0f;
+	private Vector3 pos;
+	private Transform tr;
+
+	void Start() {
+		tr = GetComponent<Transform> ();
+		pos = tr.position;
+	}
+
+	void Update() {
+		//movement shit
+		//the "pace" is too long. Somehow gotta have it move over 40px exactly.
+		if (Input.GetAxis("Horizontal")>0 && tr.position == pos) {
+			pos += Vector3.right;
+		}
+		else if (Input.GetAxis("Horizontal")<0 && tr.position == pos) {
+			pos += Vector3.left;
+		}
+		else if (Input.GetAxis("Vertical")>0 && tr.position == pos) {
+		pos += Vector3.up;
+		}
+		else if (Input.GetAxis("Vertical")<0  && tr.position == pos) {
+			pos += Vector3.down;
+		}
+	
+		transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed); 
+		//movement shit
+	}
+}
