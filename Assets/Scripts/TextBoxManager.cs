4b0e2be6d2ac7cd47896f37847bf0e280108609e
KeroseneCity <alex.mike.mark>
2017-08-25 14:46:31 -0500

    Basic system for importing text into bboxes has been implemented. Does not appear to be useful for branching conversation just yet.

--- Assets/Scripts/TextBoxManager.cs
+++ Assets/Scripts/TextBoxManager.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextBoxManager : MonoBehaviour {
 
+	public TextAsset textFile;
+	public string[] textLines;
+
+	public GameObject textBox;
+	public Text theText;
+
+	public int currLine;
+	public int endAtLine;
+
+	//public PlayerController.
+
 	// Use this for initialization
 	void Start () {
-		
+		//splits at every new line.
+		//not really what I want tbh, but we'll settle for
+		//now.
+
+		//Grab your player controller.
+
+		if (textFile != null) {
+			textLines = (textFile.text.Split ('\n'));
+		}
+
+		if (endAtLine == 0) {
+			endAtLine = textLines.Length - 2;//-1 to prevent off by one error and not use the last line.
+		}
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update(){
+		theText.text = textLines[currLine];
+		if (Input.GetKeyDown (KeyCode.Space) && currLine <= endAtLine) {
+			currLine++;
+		}
+
+		if (currLine > endAtLine) {
+			textBox.SetActive (false);
+
+		}
 	}
 }
