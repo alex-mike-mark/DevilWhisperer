f4730a2e539ae0fd9460704b05cf067748443279
KeroseneCity <alex.mike.mark>
2017-08-26 12:58:34 -0500

    More complex menuing behaviour. Next goal: create the start menu.

---
+++ Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour {
+
+	public EventSystem es;
+	public GameObject selectedObj;
+
+	private bool buttonSelected;
+
+	// Use this for initialization
+	void Start () {
+		buttonSelected = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw ("Vertical") != 0 && buttonSelected == false) {
+			es.SetSelectedGameObject (selectedObj);
+			buttonSelected = true;
+		}
+	}
+
+	private void OnDisable(){
+		buttonSelected = false;
+	}
+}
