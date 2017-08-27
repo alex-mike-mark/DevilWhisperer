ccc63c704e693ecf9751c3179e90f85190670b2b
KeroseneCity <alex.mike.mark>
2017-08-27 10:28:52 -0500

    new ConvNode creates full ConvNode now.

--- Assets/Scripts/ConvNode.cs
+++ Assets/Scripts/ConvNode.cs
@@ -15,10 +15,18 @@ public class ConvNode
 
 	public ConvNode (XElement node)
 	{
+		int i = 0;
 		prompt = (string)node.Element ("prompt");
 		//count number of response elements
+		IEnumerable<XElement> crudeResponses = node.Elements("option");
 		//create the response array of that size
+		int length = crudeResponses.Count();
+		responses = new Response[length];
 		//call new Response that many times.
+		foreach(XElement xe in crudeResponses){
+			responses[i] = new Response(xe);
+			i++;
+		}
 		id = (int)node.Element("id");
 		type = (string)node.Attribute ("Type");
 	}
--- Assets/Scripts/Response.cs
+++ Assets/Scripts/Response.cs
@@ -1,16 +1,33 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Xml.Linq;//let's try it out.
+using System.Linq;
 
+//takes in a <response> XElement, resolves the possibilities
 public class Response
 {
 	string text;
-	int weight;
 	int nextId;
 
-	public Response (string text, int weight, int nextID)
+	public Response (XElement response)
 	{
-		this.text = text;
-		this.weight = weight;
-		this.nextId = nextID;
+		text = (string)response.Element ("response");
+		IEnumerable<XElement> possibilities = response.Elements ("possibility");
+		nextId = resolvePossibility (possibilities);
+	}
+
+	//takes in all of the possibilities for a response and picks one.
+	//NOTE: IF WE WANT TO HAVE PLAYER LUCK EFFECT THIS, WE'LL NEED TO PASS IT DOWN SOMEHOW. 
+	private int resolvePossibility(IEnumerable<XElement> possibilities){
+		//yeah, this one'll get redone.
+		//the original idea is you have a weightValue modified by player luck. If that beats a weight, that one doesn't get picked.
+		//but the lightest one you don't beat does.
+		//if you beat them all, the heaviest gets picked.
+		XElement[] parr = possibilities.ToArray();
+		System.Random rng = new System.Random ();
+		return (int)parr[rng.Next (parr.Count())].Element("next");
 	}
 }
 
